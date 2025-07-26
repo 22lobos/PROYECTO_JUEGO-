127d830f947aaca8cad6850d8fde3d22f462a68e
22lobos <cr.loboso@duocuc.cl>
2025-07-25 20:19:12 -0400

    modificado

--- Pictures/ejercicos_py/juego_vava/graficos/HojaSprites.java
+++ Pictures/ejercicos_py/juego_vava/graficos/HojaSprites.java
@@ -6,6 +6,13 @@ public class HojaSprites {
     private final int ancho;
     private final int alto ;
     public final int[] pixeles;
-    public HojaSprites(final String ruta, final)
+    public HojaSprites(final String ruta, final int ancho, final int alto ){
+        this.ancho = ancho;
+        this.alto = alto;
+
+        pixeles = new int[ancho * alto ];
+        
+
+    }
     
 }
