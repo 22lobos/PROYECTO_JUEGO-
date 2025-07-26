3c66a3c632bad05225df6f14bb24c798298220f1
22lobos <cr.loboso@duocuc.cl>
2025-07-25 20:16:54 -0400

    modificado

---
+++ Pictures/ejercicos_py/juego_vava/graficos/HojaSprites.java
@@ -0,0 +1,11 @@
+
+
+
+
+public class HojaSprites {
+    private final int ancho;
+    private final int alto ;
+    public final int[] pixeles;
+    public HojaSprites(final String ruta, final)
+    
+}
