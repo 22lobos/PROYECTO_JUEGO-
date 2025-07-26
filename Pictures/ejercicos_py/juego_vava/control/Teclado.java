d42d87c5427de656437f9aa5b730d283a16552e2
22lobos <cr.loboso@duocuc.cl>
2025-07-26 14:14:52 -0400

    modificado

---
+++ Pictures/ejercicos_py/juego_vava/control/Teclado.java
@@ -0,0 +1,26 @@
+package juego_vava.control;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public final class Teclado implements KeyListener{
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+    
+}
