42221914b1913073728ac39d06f645607636d4f6
22lobos <cr.loboso@duocuc.cl>
2025-07-25 22:42:29 -0400

    modificado

--- Pictures/ejercicos_py/juego_vava/graficos/Sprite.java
+++ Pictures/ejercicos_py/juego_vava/graficos/Sprite.java
@@ -1,6 +1,15 @@
 package juego_vava.graficos;
 
 public final class Sprite {
+    private  final int lado;
+
+    private int x;
+    private int y;
+    public int[]pixeles;
+
+
+
+
      
     
 }
