3bf6db4e8c695f35f14fead5e61ad2843164b4bf
22lobos <cr.loboso@duocuc.cl>
2025-07-26 13:48:10 -0400

    modificado

--- Pictures/ejercicos_py/juego_vava/graficos/Sprite.java
+++ Pictures/ejercicos_py/juego_vava/graficos/Sprite.java
@@ -6,7 +6,19 @@ public final class Sprite {
     private int x;
     private int y;
     public int[]pixeles;
-    public Sprite(final int lado , final int columna, final int fila)
+    public Sprite(final int lado , final int columna, final int fila, final HojaSprites hoja){
+        this.lado = lado ;
+
+        pixeles = new int[lado * lado];
+
+        this.x = columna * lado;
+        this.y = fila * lado;
+
+
+
+
+
+    }
 
 
 
